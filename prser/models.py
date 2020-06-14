616840d94382a132a2c564a2ab54e1f7bcb5214d
vstakhanov <kein.sanlein@yandex.ru>
2020-06-14 15:00:46 +0300

    Added API handling

--- prser/models.py
+++ prser/models.py
@@ -6,3 +6,10 @@ class Post(models.Model):
     link = models.CharField(max_length=100, unique=True)
     text = models.TextField()
     time = models.DateTimeField()
+
+    @property
+    def anons(self):
+        return self.text[:700]
+    
+    class Meta:
+        ordering = ['-time']
