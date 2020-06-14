8b1b8f6d27a93b5cd1f70536d5801d7d8e602d50
vstakhanov <kein.sanlein@yandex.ru>
2020-06-14 17:32:58 +0300

    Corrected logging settings

--- hbrparse/settings.py
+++ hbrparse/settings.py
@@ -120,32 +120,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'error': {
-            'level': 'ERROR',
-            'class': 'logging.FileHandler',
-            'filename': 'logs/error.log',
-        },
-        'info': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': 'logs/info.log',
-        },
-    },
-    'loggers': {
-        'error': {
-            'handlers': ['error'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-        'info': {
-            'handlers': ['info'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-    },
-}
